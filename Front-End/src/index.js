8223662ea4049670374d611e86efa2c2f3f53b05
sujan <sujanpoojary884@gmail.com>
2020-07-28 12:51:03 +0530

    redux initial setup done

--- Front-End/src/index.js
+++ Front-End/src/index.js
@@ -2,15 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import authReducers from "./store/reducers/auth";
 
 import { BrowserRouter } from "react-router-dom";
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const rootReducers = combineReducers({
+  aut: authReducers,
+});
+
+const store = createStore(
+  rootReducers,
+  composeEnhancers(applyMiddleware(thunk))
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
@@ -18,4 +34,3 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ Front-End/src/store/reducers/auth.js
@@ -0,0 +1,16 @@
+const initialState = {
+  token: null,
+  loading: false,
+  error: null,
+};
+
+const authReducers = (state = initialState, action) => {
+  switch (action.type) {
+    case 1:
+      break;
+    default:
+      return state;
+  }
+};
+
+export default authReducers;
